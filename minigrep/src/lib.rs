1554604c040f7b3672b420a972a059c16e86cfd1
Jared Wang <mingsuper@gmail.com>
2023-07-22 10:08:28 +0800

    加入單元測試及使用環境變量

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,17 +1,9 @@
-use std::{fs, error::Error};
-
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    // .expect("Should havbe been able to read the file.");
-
-    println!("With text:\n{}", contents);
-    Ok(())
-}
+use std::{fs, error::Error, env};
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -24,6 +16,87 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        // 單純用 is_ok() 不嚴謹
+        // let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        // 優先權 環境變量 > 命令參數
+        let case_sensitive = env::var("CASE_INSENSITIVE").map_or_else(
+            |_| {
+            // 判斷命令參數，加入 "--case_insensitive" 則不檢查大小寫
+            !args.iter()
+                .any(|arg| arg.to_lowercase() == "--case_insensitive")
+        }, |val| val == "0" || val.to_lowercase() == "false");
+
+        Ok(Config { query, filename, case_sensitive })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let ret = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_insensitive(&config.query, &contents)
+    };
+
+    for line in ret {
+        println!("{}", line)
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut ret = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            ret.push(line);
+        }
+    }
+    ret
+}
+
+pub fn search_insensitive<'a>(
+    query: &str, 
+    contents: &'a str
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut ret = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            ret.push(line)
+        }
+    }
+    ret
+}
+
+/* Test Cases */
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive(){
+        let query = "ast";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_insensitive(query, contents));
     }
 }
